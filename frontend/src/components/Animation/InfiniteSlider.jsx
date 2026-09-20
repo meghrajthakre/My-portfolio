dea07da0934de1691db7fd076195be82c18c03b5
Meghraj Thakre <meghrajthakre444@gmail.com>
2026-09-20 13:43:21 +0530

    refactor: wrap SkillRow components in StaggerReveal and StaggerItem for enhanced animations

--- frontend/src/components/Animation/InfiniteSlider.jsx
+++ frontend/src/components/Animation/InfiniteSlider.jsx
@@ -1,17 +1,22 @@
 import { developmentSkills, workflowSkills } from "./infinite-slider/skills";
 import SkillMarquee from "./infinite-slider/SkillMarquee";
 import SkillRow from "./infinite-slider/SkillRow";
+import { StaggerItem, StaggerReveal } from "./StaggerReveal";
 
 const InfiniteSlider = () => (
   <section className="py-12 sm:py-16" aria-label="Skills">
-    <div className="flex flex-col gap-3">
-      <SkillRow title="Development" labelPosition="right">
-        <SkillMarquee skills={developmentSkills} direction="left" label="Development skills" baseSpeed={36} />
-      </SkillRow>
-      <SkillRow title="Workflow" labelPosition="left">
-        <SkillMarquee skills={workflowSkills} direction="right" label="Workflow skills" baseSpeed={30} />
-      </SkillRow>
-    </div>
+    <StaggerReveal className="flex flex-col gap-3" delay={0.06} staggerAmount={0.14}>
+      <StaggerItem>
+        <SkillRow title="Development" labelPosition="right">
+          <SkillMarquee skills={developmentSkills} direction="left" label="Development skills" baseSpeed={36} />
+        </SkillRow>
+      </StaggerItem>
+      <StaggerItem>
+        <SkillRow title="Workflow" labelPosition="left">
+          <SkillMarquee skills={workflowSkills} direction="right" label="Workflow skills" baseSpeed={30} />
+        </SkillRow>
+      </StaggerItem>
+    </StaggerReveal>
   </section>
 );
 
